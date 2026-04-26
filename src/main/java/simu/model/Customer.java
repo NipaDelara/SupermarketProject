bd87e26b2390aab04afe325d87813bbdfe26b006
NipaDelara <nipadelara@hotmail.com>
2026-04-26 22:39:40 +0300

    modify model customer

--- src/main/java/simu/model/Customer.java
+++ src/main/java/simu/model/Customer.java
@@ -3,21 +3,39 @@ package simu.model;
 import simu.framework.Clock;
 import simu.framework.Trace;
 
-
-// TODO:
-// Customer to be implemented according to the requirements of the simulation model (data!)
 public class Customer {
 	private double arrivalTime;
 	private double removalTime;
-	private int id;
+
+	//stage timestamps
+	private double shoppingStartTime;   //customer starts shopping
+	private double shoppingEndTime;     //customer finishes shopping
+	private double checkoutStartTime;   //checkout begins
+	private double checkoutEndTime;     //checkout service ends
+	private double paymentTime;         //payment is completed
+
+	private int itemCount;
+
+	private final int id;
 	private static int i = 1;
 	private static long sum = 0;
-	
+
 	public Customer() {
 	    id = i++;
-	    
+
 		arrivalTime = Clock.getInstance().getTime();
-		Trace.out(Trace.Level.INFO, "New customer #" + id + " arrived at  " + arrivalTime);
+
+        int itemCount = 10 + (int) (Math.random() * 31);
+		Trace.out(Trace.Level.INFO,
+				"New customer #" + id + " arrived at  " + arrivalTime +
+						" with trolley and " + itemCount + " items.");
+	}
+	public int getId() {
+		return id;
+	}
+
+	public int getItemCount() {
+		return itemCount;
 	}
 
 	public double getRemovalTime() {
@@ -35,7 +53,10 @@ public class Customer {
 	public void setArrivalTime(double arrivalTime) {
 		this.arrivalTime = arrivalTime;
 	}
-	
+	public double getShoppingStartTime() {
+		return shoppingStartTime;
+	}
+
 	public void reportResults() {
 		Trace.out(Trace.Level.INFO, "\nCustomer " + id + " ready! ");
 		Trace.out(Trace.Level.INFO, "Customer "   + id + " arrived: " + arrivalTime);
